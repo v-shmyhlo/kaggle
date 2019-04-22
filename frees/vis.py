baad3fbf42ecf35ee9d54e24aac0252809dad542
Vlad Shmyhlo <aion.stu@gmail.com>
2019-04-22 16:07:50 +0300

    Update

---
+++ frees/vis.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile
+import scipy.signal
+import librosa
+
+sig, rate = soundfile.read('./frees/sample.wav', dtype=np.float32)
+sig = sig[:, 0]
+
+n_fft = round(0.025 * rate)  # TODO: refactor
+hop_length = round(0.01 * rate)  # TODO: refactor
+
+print(n_fft, hop_length)
+print(n_fft * (1 / rate), hop_length * (1 / rate))
+
+# x = librosa.feature.melspectrogram(sig, n_fft=n_fft, hop_length=hop_length)
+x = librosa.core.stft(sig, n_fft=n_fft, hop_length=hop_length)
+x = np.abs(x)
+x = np.log(x)
+x = np.dot(librosa.filters.mel(rate, n_fft), x)
+x = (x - x.mean(1, keepdims=True)) / x.std(1, keepdims=True)
+
+print(x.shape, x.dtype)
+print(x.min(), x.max(), x.std())
+
+plt.hist(x.ravel(), bins=100)
+plt.show()
+
+plt.imshow(x[:, :500])
+plt.show()
